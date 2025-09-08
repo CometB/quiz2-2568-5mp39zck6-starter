52285185af37ae8702cac99ede07af8abd7805ca
CometB <rpila23461@gmail.com>
2025-09-08 10:51:17 +0700

    .

--- src/pages/TrackerPage.tsx
+++ src/pages/TrackerPage.tsx
@@ -1,18 +1,16 @@
-import { useState } from "react";
+// import { useState } from "react";
 import { Button, Stack, Title, Divider, Container } from "@mantine/core";
-import { v4 as uuidv4 } from "uuid";
-import AddExpenseModal from "../components/Modal";
 
-type Expense = {
-  id: string;
-  name: string;
-  amount: number | string;
-  category: string;
-};
+// type Expense = {
+//   id: string;
+//   name: string;
+//   amount: number | string;
+//   category: string;
+// };
 
 export default function ExpenseTracker() {
-  const [opened, setOpened] = useState(false);
-  const [expenses, setExpenses] = useState<Expense[]>([]);
+  // const [opened, setOpened] = useState(false);
+  // const [expenses, setExpenses] = useState<Expense[]>([]);
   const categories = ["Food", "Transport", "Entertainment"];
 
   return (
@@ -33,7 +31,7 @@ export default function ExpenseTracker() {
 
       <Divider my="md" />
       {/* Type additional card here. */}
-      <Stack>{/* Type additional expense card list here. */}</Stack>
+      <Stack></Stack>
     </Container>
   );
 }
